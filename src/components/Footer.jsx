1445d81ad84167230fd8512466f25bb56f64d821
Acousticom <omkarsrinivasa.2000@gmail.com>
2024-02-28 21:52:09 +0530

    ui fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { FaGlobe,FaFacebook,FaPhoneAlt } from "react-icons/fa";
+import React from "react";
+import { FaGlobe, FaFacebook, FaPhoneAlt } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className='bg-red-500 pt-7 pb-14 flex flex-col items-center gap-5 md:flex-row md:justify-around mt-6'>
-      <p className='flex items-end gap-4 text-white font-semibold'><span className='rounded-full bg-white'><FaPhoneAlt size={35} className='text-red-500 px-2 py-2'/></span>www.facebook.com/cripumps</p>
-        <p className='flex items-end gap-4 text-white font-semibold'><FaFacebook size={35} color='white'/>www.facebook.com/cripumps</p>
-        <p className='flex items-end gap-4 text-white font-semibold'><FaGlobe size={35} color='white'/>www.crigroups.com</p>
+    <div className="bg-red-500 pt-7 pb-14 mt-6 flex justify-center">
+      <div className="md:flex md:justify-around w-fit md:w-full ">
+        <p className="flex items-end gap-2 text-white font-semibold my-3">
+          <span className="rounded-full bg-white">
+            <FaPhoneAlt size={35} className="text-red-500 px-2 py-2" />
+          </span>
+          Toll free <strong>1800 200 1234</strong>
+        </p>
+        <p className="flex items-end gap-4 text-white font-semibold my-3">
+          <FaFacebook size={35} color="white" />
+          www.facebook.com/cripumps
+        </p>
+        <p className="flex items-end gap-4 text-white font-semibold my-3">
+          <FaGlobe size={35} color="white" />
+          www.crigroups.com
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
